dee004f7099521d77a0e2e0641edefb36e19cbbf
Sergei Papulin <papulin_hse@mail.ru>
2021-05-02 19:19:28 +0300

    adjust plot size

--- lib/plot_utils.py
+++ lib/plot_utils.py
@@ -28,10 +28,10 @@ class CPlot:
         x2_min = np.min([X_train[:,1].min(), X_test[:,1].min()])
         x2_max = np.max([X_train[:,1].max(), X_test[:,1].max()])
 
-        x1_min = x1_min + (0.1*x1_min)
-        x1_max = x1_max + (0.1*x1_max)
-        x2_min = x2_min + (0.1*x2_min)
-        x2_max = x2_max + (0.1*x2_max)
+        x1_min = x1_min - (0.1*np.abs(x1_min))
+        x1_max = x1_max + (0.1*np.abs(x1_max))
+        x2_min = x2_min - (0.1*np.abs(x2_min))
+        x2_max = x2_max + (0.1*np.abs(x2_max))
 
         xx, yy = np.meshgrid(np.arange(x1_min, x1_max, step), 
                              np.arange(x2_min, x2_max, step))
@@ -60,6 +60,8 @@ class CPlot:
         plt.scatter(X_train[:,0], X_train[:,1], c=model.predict(X_train), s=20, cmap=cmap)
         plt.xlabel("X1")
         plt.ylabel("X2")
+        plt.xlim(x1_min, x1_max)
+        plt.ylim(x2_min, x2_max)    
         plt.legend(*scatter.legend_elements(), title="Class:")
         plt.grid(True)
 
@@ -70,11 +72,62 @@ class CPlot:
         plt.scatter(X_test[:,0], X_test[:,1], c=model.predict(X_test), s=20, cmap=cmap)
         plt.xlabel("X1")
         plt.ylabel("X2")
+        plt.xlim(x1_min, x1_max)s
+        plt.ylim(x2_min, x2_max)    
         plt.legend(*scatter.legend_elements(), title="Class:")
         plt.grid(True)
 
         plt.show()
         
+    @staticmethod
+    def show_prediction_plot(model, X, y, title=None, cmap="tab10", proba=False):
+
+        step = 0.01
+
+        x1_min = np.min([X[:,0].min(), X[:,0].min()])
+        x1_max = np.max([X[:,0].max(), X[:,0].max()])
+
+        x2_min = np.min([X[:,1].min(), X[:,1].min()])
+        x2_max = np.max([X[:,1].max(), X[:,1].max()])
+
+        x1_min = x1_min - (0.1*np.abs(x1_min))
+        x1_max = x1_max + (0.1*np.abs(x1_max))
+        x2_min = x2_min - (0.1*np.abs(x2_min))
+        x2_max = x2_max + (0.1*np.abs(x2_max))
+
+        xx, yy = np.meshgrid(np.arange(x1_min, x1_max, step), 
+                             np.arange(x2_min, x2_max, step))
+        points = np.c_[xx.ravel(), yy.ravel()]
+
+        if proba is True and hasattr(model, "predict_proba") and len(model.classes_) == 2:
+            cmap = cm.bwr
+            Z = model.predict_proba(points)[:, 1]
+        elif proba is True and hasattr(model, "decision_function") and len(model.classes_) == 2:
+            cmap = cm.bwr
+            Z = model.decision_function(points)
+        else:
+            Z = model.predict(points)
+        
+        Z = Z.reshape(xx.shape)
+
+        plt.figure(1, figsize=[6, 4])
+
+        if title:
+            plt.suptitle(title, fontsize=16)
+
+        plt.subplot(1,1,1)
+        plt.title("Train data")
+        plt.contourf(xx, yy, Z, cmap=cmap, alpha=.5)
+        scatter = plt.scatter(X[:,0], X[:,1], c=y, s=80, cmap=cmap, alpha=0.5)
+        plt.scatter(X[:,0], X[:,1], c=model.predict(X), s=20, cmap=cmap)
+        plt.xlabel("X1")
+        plt.ylabel("X2")
+        plt.xlim(x1_min, x1_max)
+        plt.ylim(x2_min, x2_max)                   
+        plt.legend(*scatter.legend_elements(), title="Class:")
+        plt.grid(True)
+
+        plt.show()
 
 class RPlot:
     """Regression plot class with static methods"""
