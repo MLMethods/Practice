b9558a28a97f50043c8154a5f78980e31284005e
shadowk1337 <r_fyodorov2001@mail.ru>
2025-02-06 03:21:19 +0300

    remove repos sync by default

--- env/docker/entrypoint.sh
+++ env/docker/entrypoint.sh
@@ -10,18 +10,6 @@ if [ -f "$ENV_FILE" ]; then
     set +o allexport
 fi
 
-# echo "Running entrypoint script..."
-
-# if [[ ! -d "$PRACTICE_REPO" || ! -d "$ASSIGNMENTS_REPO" ]]; then
-#     echo "Set the path where repo will be created in .env"
-# fi
-
-# echo "Cloning Practice repository..."
-# git clone https://github.com/MLMethods/Practice "$PRACTICE_REPO"
-
-# echo "Cloning Assignments repository..."
-# git clone https://github.com/MLMethods/Assignments "$ASSIGNMENTS_REPO"
-
 source /opt/conda/etc/profile.d/conda.sh
 conda activate mlmethods
 
